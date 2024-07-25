3f2721630cd6c055f3158bd4a5ac6636c523e5ae
mustlive <mustlive@gmail.com>
2024-07-25 21:15:52 +0300

    Added in classes Rabbit, duck, egg, needle, deAth @Component, constructor, final field Changed in upper classes method toString ""-> down class.toString()

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private final Death8 deth;
+
+    @Autowired
+    public Needle7(Death8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
